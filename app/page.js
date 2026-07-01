84a91b228c0631f1026cac44b8c4c45f23786462
hakiluxa-cloud <hakiluxa@gmail.com>
2026-07-01 22:41:59 +0800

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import ChatBox from '../components/ChatBox';
+
+// Fungsi Page merender halaman utama aplikasi dan menampilkan komponen ChatBox.
+export default function Page() {
+  return (
+    <main>
+      <ChatBox />
+    </main>
+  );
+}
